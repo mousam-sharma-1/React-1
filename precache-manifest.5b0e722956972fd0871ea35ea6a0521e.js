f3cc4f707cc8f3e4b5686b8951a5a2dc33ce465d
mousam sharma <mousam.sharma13@gmail.com>
2019-11-08 23:05:02 +0530

    Updates

---
+++ precache-manifest.5b0e722956972fd0871ea35ea6a0521e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae254a049fd93c643001cf87b5a6f16",
+    "url": "/React-1/index.html"
+  },
+  {
+    "revision": "56d0218355d3776b700e",
+    "url": "/React-1/static/css/main.d2710392.chunk.css"
+  },
+  {
+    "revision": "846f8dea11d893d2d675",
+    "url": "/React-1/static/js/2.291b84c3.chunk.js"
+  },
+  {
+    "revision": "56d0218355d3776b700e",
+    "url": "/React-1/static/js/main.cd272619.chunk.js"
+  },
+  {
+    "revision": "6e9040a2e867969b2cad",
+    "url": "/React-1/static/js/runtime-main.58fb3bcc.js"
+  }
+]);
